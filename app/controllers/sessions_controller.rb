08eb16a6558623d4e4443e087d3f4807b56f008c
yvonnersz <yvonnersz@gmail.com>
2020-07-13 22:40:39 -0700

    add root route

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,13 @@
+class SessionsController < ApplicationController
+
+    def new
+        render '/login'
+    end
+
+    def create
+    end
+
+    def destroy
+    end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
 
+  root 'sessions#new'
+
   get '/signup' => 'customers#new'
   post '/signup' => 'customers#create'
 
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+
   resources :customers
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
 end
