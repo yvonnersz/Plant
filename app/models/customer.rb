3d59239eb066fb43b8411c2035fe37fdd3be92c2
yvonnersz <yvonnersz@gmail.com>
2020-07-13 19:28:50 -0700

    add has_many association

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,2 +1,4 @@
 class Customer < ApplicationRecord
+    has_many :plants
+    has_many :stores, through: :plants
 end
--- app/models/store.rb
+++ app/models/store.rb
@@ -1,2 +1,4 @@
 class Store < ApplicationRecord
+    has_many :plants
+    has_many :customers, through: :plants
 end
