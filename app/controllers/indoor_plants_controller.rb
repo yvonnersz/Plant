e6a40f599d58b91d98727d97f3e5e1d8af20e5ce
yvonnersz <yvonnersz@gmail.com>
2020-07-14 17:51:55 -0700

    build #new and #create for IndoorPlantsController

--- app/controllers/indoor_plants_controller.rb
+++ app/controllers/indoor_plants_controller.rb
@@ -1,12 +1,15 @@
 class IndoorPlantsController < ApplicationController
 
     def new
+        @indoor_plant = IndoorPlant.new
     end
 
     def create
+        @indoor_plant = IndoorPlant.new
+        redirect_to @indoor_plant
     end
 
     def show
     end
-    
+
 end
