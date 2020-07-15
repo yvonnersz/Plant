91409357dc9d8cba0dd16961f8678e11aafd1cce
yvonnersz <yvonnersz@gmail.com>
2020-07-14 17:43:17 -0700

    add stores#new and stores#create

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -1,12 +1,21 @@
 class StoresController < ApplicationController
 
     def new
+        @store = Store.new
     end
 
     def create
+        @store = Store.create(store_params)
+        redirect_to @store
     end
 
     def show
+    end
+
+    private
+
+    def store_params
+        params.require(:store).permit(:name, :prestige)
     end
 
 end
