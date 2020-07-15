fba8bb8be4ef4ec10388beefd68afa5a1d932f98
yvonnersz <yvonnersz@gmail.com>
2020-07-14 17:55:15 -0700

    add private method, #indoor_plants_params

--- app/controllers/indoor_plants_controller.rb
+++ app/controllers/indoor_plants_controller.rb
@@ -5,11 +5,17 @@ class IndoorPlantsController < ApplicationController
     end
 
     def create
-        @indoor_plant = IndoorPlant.new
+        @indoor_plant = IndoorPlant.create(indoor_plants_params)
         redirect_to @indoor_plant
     end
 
     def show
+    end
+
+    private
+
+    def indoor_plants_params
+        params.require(:indoor_plants).permit(:name, :price)
     end
 
 end
