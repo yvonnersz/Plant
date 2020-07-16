c36bb67f28b57fc20a1e8f4a8e2ef01208a08568
yvonnersz <yvonnersz@gmail.com>
2020-07-15 23:32:46 -0700

    add helper methods to correct file

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,15 +3,5 @@ class ApplicationController < ActionController::Base
     def welcome
         render '/welcome'
     end
-
-    helpers do
-        def current_customer
-            @current_customer ||= Customer.find_by(:id => session[:customer_id])
-        end
-
-        def logged_in?
-            !!current_customer
-        end
-    end
-            
+     
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+    def current_customer
+        @current_customer ||= Customer.find_by(:id => session[:customer_id])
+    end
+
+    def logged_in?
+        !!current_customer
+    end
+    
 end
