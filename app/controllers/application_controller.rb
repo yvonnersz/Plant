e67714b0eb36127af58ac256e88d7ece9763442f
yvonnersz <yvonnersz@gmail.com>
2020-07-21 04:58:11 -0700

    add about section

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,5 +3,9 @@ class ApplicationController < ActionController::Base
     def welcome
         render '/welcome'
     end
+
+    def about
+        render '/about'
+    end
      
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
 
   get '/' => 'application#welcome'
 
+  get '/about' => 'application#about'
+
   get '/signup' => 'customers#new'
   post '/signup' => 'customers#create'
 
