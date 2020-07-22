41af162bbb7d2a6246e732572e19621410f15f85
yvonnersz <yvonnersz@gmail.com>
2020-07-22 03:34:56 -0700

    build #plants_sold method

--- app/models/store.rb
+++ app/models/store.rb
@@ -3,4 +3,15 @@ class Store < ApplicationRecord
     
     has_many :indoor_plants
     belongs_to :customer
+
+    def plants_sold
+        x = []
+
+        Customer.all.each do |customer|
+            customer.indoor_plants.collect {|plant| x << plant.store_id if plant.store_id == self.id}
+        end
+
+        x.count
+    end
+
 end
