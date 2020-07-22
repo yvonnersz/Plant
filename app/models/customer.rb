408f1e83d4dd8e53977a0030fecf4e8e4df79c43
yvonnersz <yvonnersz@gmail.com>
2020-07-21 21:13:37 -0700

    validates cash to be greater than 0

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,9 +1,10 @@
 class Customer < ApplicationRecord
+    validates :name, :email, presence: true
+    validates :name, :email, uniqueness: true
+    validates :cash, numericality: { greater_than: 0 }
+
     has_many :customer_indoor_plants
     has_many :indoor_plants, through: :customer_indoor_plants
     has_secure_password
     has_many :stores
-
-    validates :name, :email, presence: true
-    validates :name, :email, uniqueness: true
 end
