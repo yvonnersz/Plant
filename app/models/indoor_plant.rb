9f181af94c47a7b8ff65dd808951e1b7f87f6add
yvonnersz <yvonnersz@gmail.com>
2020-07-22 01:59:19 -0700

    add name validation to IndoorPlant model

--- app/models/indoor_plant.rb
+++ app/models/indoor_plant.rb
@@ -1,8 +1,8 @@
 class IndoorPlant < ApplicationRecord
+  validates :name, uniqueness: true
+
   belongs_to :store
   has_many :customer_indoor_plants
   has_many :customers, through: :customer_indoor_plants
  
-
-
 end
