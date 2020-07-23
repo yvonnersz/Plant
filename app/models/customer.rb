79d2651267d30330aec84c75ff657970ff237c78
yvonnersz <yvonnersz@gmail.com>
2020-07-22 23:06:15 -0700

    Customer association relationship rearrangement

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,7 +3,7 @@ class Customer < ApplicationRecord
     validates :cash, numericality: { greater_than_or_equal_to: 0 }
     has_secure_password
 
+    has_many :stores
     has_many :customer_indoor_plants
     has_many :indoor_plants, through: :customer_indoor_plants
-    has_many :stores
 end
